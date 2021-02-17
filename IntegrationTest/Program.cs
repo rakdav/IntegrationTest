9f57d9968b309c4201620650ae8d7d90177286c5
John Doe <gogs@fake.local>
2021-02-17 11:01:22 +0200

    Добавьте файлы проекта.

---
+++ IntegrationTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MathLibrary;
+namespace IntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(double x=-10;x<=10;x+=0.5)
+            {
+                Console.WriteLine(x+"        "+String.Format("{0:f3}",(ClassMath.Sec(x)+ClassMath.CoSec(x)))+"              "+String.Format("{0:f3}",((1/Math.Cos(x))+(1/Math.Sin(x)))));
+            }
+        }
+    }
+}
---
+++ MathLibrary/ClassMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class ClassMath
+    {
+        public static double Sec(double x)
+        {
+            int n = 1;
+            double S = 0;
+            double dx=0;
+            while(dx<0.001)
+            {
+                dx = (Math.Pow(-1, n - 1)) * Math.Pow(x, 2 * n - 1) / fact(2 * n - 1);
+                n++;
+                S += dx;
+            }
+            return 1 / S;
+        }
+        public static double CoSec(double x)
+        {
+            int n = 0;
+            double S = 0;
+            double dx = 0;
+            while (dx < 0.001)
+            {
+                dx = (Math.Pow(-1, n)) * Math.Pow(x, 2 * n) / fact(2 * n);
+                n++;
+                S += dx;
+            }
+            return 1 / S;
+        }
+        public static long fact(int n)
+        {
+            long F = 1;
+            for (int i = 1; i <= n; i++) F *= i;
+            return F;
+        }
+    }
+}
